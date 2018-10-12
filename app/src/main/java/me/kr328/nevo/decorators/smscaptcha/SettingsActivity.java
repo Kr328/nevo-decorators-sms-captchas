0c9bf9eedec6e640415bb0d961a616ee0a71eb88
Kr328 <39107975+Kr328@users.noreply.github.com>
2018-10-12 17:35:27 +0800

    version 1.8

--- app/src/main/java/me/kr328/nevo/decorators/smscaptcha/SettingsActivity.java
+++ app/src/main/java/me/kr328/nevo/decorators/smscaptcha/SettingsActivity.java
@@ -1,10 +1,16 @@
 package me.kr328.nevo.decorators.smscaptcha;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import me.kr328.nevo.decorators.smscaptcha.utils.PackageUtils;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -21,9 +27,10 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
 
         setupWindowOreo();
+
+        new Thread(this::detectedNevolution).start();
     }
 
     private void setupWindowOreo() {
@@ -31,5 +38,19 @@ public class SettingsActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(decorView.getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
         getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimary ,getTheme()));
+    }
+
+    private void detectedNevolution() {
+        if ( !PackageUtils.hasPackageInstalled(this ,Global.NEVOLUTION_PACKAGE_NAME) ) {
+            runOnUiThread(() -> new AlertDialog.Builder(this).
+                    setTitle(R.string.missing_nevolution_dialog_title).
+                    setMessage(R.string.missing_nevolution_dialog_message).
+                    setOnDismissListener(dialog -> this.finish()).
+                    setPositiveButton(R.string.missing_nevolution_dialog_button ,(dialog ,i) -> startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("market://details?id=" + Global.NEVOLUTION_PACKAGE_NAME)))).
+                    show());
+        }
+        else {
+            runOnUiThread(() -> setContentView(R.layout.main_layout));
+        }
     }
 }
